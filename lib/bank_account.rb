10bd8fb7529807624eb06bbf98e5fd659e9dabf9
Cameron Hudson <camnn3@yahoo.com>
2020-08-12 21:16:03 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,26 +1,27 @@
 class BankAccount
   attr_accessor :balance, :status
   attr_reader :name
-
+  
   def initialize(account_name)
     @name = account_name
     @balance = 1000
     @status = "open"
   end
-
-  def deposit(money)
-    @balance += money
+  
+  def deposit(total)
+    @balance += total
   end
-
+  
   def display_balance
     "Your balance is $#{@balance}."
   end
-
+  
   def valid?
-    @status == "open" && @balance > 0 ? true : false
+    status == "open" && balance > 0
+  end
+  
+  def close_account
+    self.status = "closed"
   end
 
-  def close_account
-    @status = "closed"
-  end
 end
